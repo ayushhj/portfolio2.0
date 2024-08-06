72b04e2e9eedbf4d42e466dfd0ccf3e0e5de079e
ayushhj <ayushjoshi494@gmail.com>
2024-08-06 23:56:19 +0530

    created basic layout for skills section

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
           <Navbar />
           <Body />
           <Skills/>
-          {/* <Footer/> */}
+          <Footer/>
           
       </div>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,25 +1,73 @@
 import React, { useState } from "react";
 
-import arrow from "../assets/jslogoinactive.png";
-import arrowHover from "../assets/jslogoactive.png";
+import arrow from "../assets/jsinactivelogo.png";
+import arrowHover from "../assets/jsactivelogo.png";
+import reactactive from "../assets/reactlogo.png";
+import reactinactive from "../assets/reactinactivelogo.png";
 
 const Skills = () => {
   const [over, setOver] = useState(false);
+  const [active, setActive] = useState(false);
 
   return (
-    <div className="skillscontainer">
-      <h3>What I Code With !</h3>
 
-      <div >
-        <img 
-        onMouseOver={() => setOver(true)} onMouseOut={() => setOver(false)}
-          src={over ? arrowHover : arrow}
-          alt="arrow"
-          width="400"
-          height="400"
-        ></img>
+<div className="company-logos">
+      <h4 className="skillsheading" >Some of the technologies I code with!</h4>
+      <div className="logos-container">
+        <div className="logo">
+        <img
+             className="logoimg"
+             onMouseOver={() => setOver(true)}
+             onMouseOut={() => setOver(false)}
+             src={over ? arrowHover : arrow}
+          />
+
+        </div>
+        <div className="logo">
+        <img
+             className="logoimg"
+             onMouseOver={() => setActive(true)}
+             onMouseOut={() => setActive(false)}
+             src={active ? reactactive : reactinactive}
+          ></img>
+        </div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+
       </div>
     </div>
+
+    // <div className="skillsmaincontainer">
+    //   <div className="skillsheading">
+    //     <h3>What I Code With !</h3>
+    //   </div>
+
+    //   <div className="skillscontainer">
+    //     <div className="techlogos">
+    //       <img
+    //         className="logoimg"
+    //         onMouseOver={() => setOver(true)}
+    //         onMouseOut={() => setOver(false)}
+    //         src={over ? arrowHover : arrow}
+    //       ></img>
+    //     </div>
+    //     <div className="techlogos">
+    //       <img
+    //         className="logoimg"
+    //         onMouseOver={() => setActive(true)}
+    //         onMouseOut={() => setActive(false)}
+    //         src={active ? reactactive : reactinactive}
+    //       ></img>
+    //     </div>
+    //     s
+    //   </div>
+    // </div>
   );
 };
 
